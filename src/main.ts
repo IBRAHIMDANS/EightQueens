2ca27f1bc4695f21ccf09a7c6fe5209a4705c2cb
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-09-16 15:50:09 +0200

    fld

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env ts-node
 
-(function main(file: string = process.argv[2]) {
-    console.log('ook');
+(function main(queens: number = 8, colums: number = 8) {
+
+    const chessboard = [0, 1, 2, 3, 4, 5, 6, 7];
+    for (const chessboardKey in chessboard) {
+    }
+
 })();
